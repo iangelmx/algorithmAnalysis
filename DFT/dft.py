b35b57af3d9c60310ae9de71f9e5cf687c69882b
iangelmx <iangelmx@hotmail.com>
2018-09-28 00:03:34 -0500

    Se añade la FFT

--- DFT/dft.py
+++ DFT/dft.py
@@ -15,16 +15,34 @@ def dft(Xn):
 	return arreglo
 
 # def dft(xs):
-#     "naive dft"
-#     n = len(xs)
-#     return [sum((xs[k] * iexp(-2 * math.pi * i * k / n) for k in range(n)))
-#             for i in range(n)]
+#	 "naive dft"
+#	 n = len(xs)
+#	 return [sum((xs[k] * iexp(-2 * math.pi * i * k / n) for k in range(n)))
+#			 for i in range(n)]
 
 """def dftinv(Xn):
 	"naive dft"
 	n = len(Xn)
 	return [sum((Xn[k] * expI(2 * math.pi * i * k / n) for k in range(n))) / n
 			for i in range(n)]"""
+def is_pow2(n):
+	return False if n == 0 else (n == 1 or is_pow2(n >> 1))
+
+def fft_(xs, n, start=0, stride=1):
+	"cooley-turkey fft"
+	if n == 1:
+		return [xs[start]]
+	hn, sd = n // 2, stride * 2
+	rs = fft_(xs, hn, start, sd) + fft_(xs, hn, start + stride, sd)
+	for i in range(hn):
+		e = expI(-2 * math.pi * i / n)
+		rs[i], rs[i + hn] = rs[i] + e * rs[i + hn], rs[i] - e * rs[i + hn]
+		pass
+	return rs
+
+def fft(xs):
+	assert is_pow2(len(xs))
+	return fft_(xs, len(xs))
 
 if __name__ == "__main__":
 	wave = [0, 1, 2, 3, 3, 2, 1, 0]
