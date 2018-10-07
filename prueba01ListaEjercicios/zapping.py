148e17917a58476413543c1b02bdcea4c5b093e1
iangelmx <iangelmx@hotmail.com>
2018-10-07 01:55:47 -0500

    Se añade el programa zapping

--- prueba01ListaEjercicios/zapping.py
+++ prueba01ListaEjercicios/zapping.py
@@ -1,5 +1,22 @@
 try:
-	pass
+	a = 0
+	while True:
+		a+=1
+		[current, destination] = list(map(int,  input("").split() ))
+		if current == -1 and destination == -1:
+			break
+
+		toUp = abs( destination - current )
+
+		if destination > current:
+			toDown = abs( current + 1 +(99 - destination) )
+		else:
+			toDown = abs( destination + 1 +(99 - current) )
+
+		if toUp < toDown:
+			print(toUp)
+		else:
+			print(toDown)
+
 except Exception as ex:
-	print(ex)
 	pass
